1beb8988e4d288d70e0c317d18a16ec7b7d1a5d5
Get-Bad <92388856+Get-Bad@users.noreply.github.com>
2025-10-14 13:42:53 +0300

    Delete lab3/blog directory

--- lab3/blog/articles/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Article
-
-def archive(request):
-    posts = Article.objects.all()
-    return render(request, 'archive.html', {"posts": posts})
